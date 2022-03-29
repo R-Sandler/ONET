bf922b7d00cf740627e88ab72662df234194f7dd
Rachel Sandler <sandler@nscl.msu.edu>
2022-03-29 14:21:08 -0400

    Added knowledge questions

--- OnetPlugin.py
+++ OnetPlugin.py
@@ -12,26 +12,31 @@ def Search(self):
     career = ui.searchField.text()
     row = careerList[careerList["Title"]==career]
     code = row["Code"].to_string(header=False, index=False)
-    searchResults = onet_ws.call('mnm/careers/'+code+"/skills")
+    skillResults = onet_ws.call('mnm/careers/'+code+"/skills")
     count = 0
     total = 0
-    introQuestion = random.randint(0,11)
-    ui.questionTable.setItem(0,0,QtWidgets.QTableWidgetItem(questionDoc[0][introQuestion].text))
+    ui.questionTable.setItem(0,0,QtWidgets.QTableWidgetItem(questionDoc[0][1].text))
+    introQuestion = random.randint(1,11)
+    ui.questionTable.setItem(1,0,QtWidgets.QTableWidgetItem(questionDoc[0][introQuestion].text))
     questionSelection = [random.randint(0,11) for i in range(0,5)]
+    knowledgeResults = onet_ws.call('mnm/careers/'+code+"/knowledge")
     try:
-        for item in searchResults['group']:
-            while total<5:
+        while total<5:
+            for item in skillResults['group']:
                 for skill in item['element']:
                     skillList.append(skill['id'])
                     if count < 5:
                         for skill in questionDoc.findall('skill'):
                             if skill.find('title').get('id') == skillList[count]:
                                 questions = skill.findall('element')
-                                ui.questionTable.setItem(count+1,0, QtWidgets.QTableWidgetItem(questions[questionSelection[count]].text))
+                                ui.questionTable.setItem(count+2,0, QtWidgets.QTableWidgetItem(questions[questionSelection[count]].text))
                     count += 1
                     total += 1
+        ui.questionTable.setItem(7,0,QtWidgets.QTableWidgetItem("What education, work experience, or other credentials do you have to demonstrate your knowledge of "+knowledgeResults['group'][0]['element'][0]['name']+"?"))
+        ui.questionTable.setItem(8,0,QtWidgets.QTableWidgetItem("What technical knowledge or skill do you have in respect to "+knowledgeResults['group'][1]['element'][0]['name']+"?"))
+        
     except:
-        for i in range (0,10):
+        for i in range (1,10):
             ui.questionTable.setItem(i,0,QtWidgets.QTableWidgetItem(questionDoc[0][i+1].text))
     ui.questionTable.resizeColumnsToContents()
 
@@ -66,19 +71,9 @@ if __name__ == "__main__":
     onet_ws = OnetWebService("harqen_ai", "2298huc")
 
     global questionDoc
-    #questionDoc = ET.parse('questions')
-    #for info in questionDoc.findall('.//skill/title'):
-
-    #questionDoc = pd.read_xml('questions')
-    #print(questionDoc)
 
     tree = ET.parse('questions')
     questionDoc = tree.getroot()
-    #for child in root:
-    #    print(child.tag, child.attrib)
-    #for skill in questionDoc.findall('skill'):
-    #    print(skill.find('title').get('id'))
-    
 
 
     careerList = GetCareerList()
